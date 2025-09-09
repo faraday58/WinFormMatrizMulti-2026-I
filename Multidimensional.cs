94686a8da0e0a06b4b4057322de150cf9aa6d2db
faraday58 <jarodriguezvera@gmail.com>
2025-09-08 21:31:14 -0600

    Add sum, read and toString

--- Multidimensional.cs
+++ Multidimensional.cs
@@ -29,9 +29,61 @@ namespace WinFormMatrizMulti_2026_I
             string[] rows = matrix.Split(';'); // ["1,2","3,4"]
             string[] columns = rows[0].Split(','); // ["1","2"]
 
+            Multidimensional m1 = new Multidimensional(rows.Length, columns.Length);
+
+            for (int i = 0; i < m1.M; i ++)
+            {
+                string[] values = rows[i].Split(','); // ["1","2"]
+                for (int j = 0; j < m1.N; j++)
+                {
+                    m1.matrix[i, j] = int.Parse(values[j]);
+                }
+            }
+
+            return m1;
+
+        }
+
+
+        public static Multidimensional operator + (Multidimensional m1, Multidimensional m2)
+        {
+
+            // 6 8     5 6
+            // 10 12     7 8
+            Multidimensional m3 = new Multidimensional(m1.M, m1.N);
+
+            for (int i = 0; i < m1.M; i++)
+            {
+                for(int j = 0; j < m1.N; j++)
+                {
+                    m3.matrix[i,j] = m1.matrix[i, j] + m2.matrix[i, j];
+                }
+
+            }
+            return m3;
+
+        }
 
 
 
+        public override string ToString()
+        {
+            // 1 2
+            // 3 4
+
+            string matrix = "";
+
+            for (int i = 0; i < this.M; i++)
+            {
+                for (int j = 0; j < this.N; j++)  
+                {
+                    matrix += this.matrix[i, j] + "\t"; 
+                }
+                matrix += "\n";
+            }
+
+
+            return matrix; 
 
         }
 
